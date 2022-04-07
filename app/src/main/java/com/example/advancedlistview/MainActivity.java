c6e758b00afff69845f7bc5698b32fc971ab8eb1
SachPrecious <sachithrabandara49@gmail.com>
2022-04-07 20:42:47 +0530

    Commit2

--- app/src/main/java/com/example/advancedlistview/MainActivity.java
+++ app/src/main/java/com/example/advancedlistview/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String title[]={"Donald Trump","Joe Biden","Lionel Messi","John Rawls","Neymar","Ronaldo","Ozil","Vladimir Putin","Queen Elizabeth","Rihanna","Selena Gomez","Messi","Salah","Sophia Loren","Kane","Taylor Swift"};
+        String description[]={"0778855963"};
+        
+
     }
 }
